2ec6d99cd84b864540852206581f92c2a7832f1c
Lauren M Hale <laurenmhale@Laurens-MacBook-Pro.local>
2018-03-06 15:14:11 -0500

    adding game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,18 +2,22 @@
 var cards = ["queen","queen","king","king"];
 var cardsInPlay = [];
 
-var cardOne = cards[2];
-
-cardsInPlay.push(cardOne);
-
-var cardTwo = cards[3];
-
-cardsInPlay.push(cardTwo);
-
-if (cardsInPlay.length === 2){
+function checkForMatch(){
   if(cardsInPlay[0] === cardsInPlay[1]) {
    alert ("You found a match!");
  }else{
    alert("Sorry, try again");
  }
 }
+
+function flipCard (cardId) {
+if (cardsInPlay.length === 2){
+
+}
+ cardsInPlay.push(cards[cardId]);
+checkForMatch();
+console.log("User flipped " + cards[cardId]);
+
+}
+flipCard(0);
+flipCard(2);
